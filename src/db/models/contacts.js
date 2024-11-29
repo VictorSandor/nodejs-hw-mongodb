3e7c15f323f489b89f664620386b1e2bd816d2fc
Viktor S <sandorikviktor@gmail.com>
2024-11-29 11:08:16 +0200

    contacts

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,35 +1,19 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 
-const contactsSchema = new Schema(
+const ContactsSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavourite: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
+    name: { type: String, required: true },
+    phoneNumber: { type: String, requared: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
     contactType: {
       type: String,
-      required: true,
-      enum: ['work', 'home', 'personal'],
-      default: 'personal',
+      requared: true,
+      enum: ["work", "home", "personal"],
+      default: "personal",
     },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  { timestamps: true, versionKey: false }
 );
 
-export const ContactsCollection = model('contacts', contactsSchema);
+export const ContactsCollection = model("contacts", ContactsSchema);
