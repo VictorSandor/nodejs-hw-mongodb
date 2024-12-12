1cc6fccf99baceb70bf9cc936e5d3bfc2f73a5bb
Viktor S <sandorikviktor@gmail.com>
2024-12-13 01:16:50 +0200

    valid

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,10 +1,10 @@
 import createHttpError from "http-errors";
 
-export const validateBody = (schema) => async (req, _res, next) => {
+export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(body, { abortEarly: false });
     const userId = req.user?._id.toString();
     const body = userId ? { userId, ...req.body } : req.body;
+    await schema.validateAsync(body, { abortEarly: false });
     next();
   } catch (err) {
     console.log(err, "err in validate body", req.body, "req.body");
