a2fc7263734b6f4532143ddb7e03ff3a458c20ea
Viktor S <sandorikviktor@gmail.com>
2025-01-04 16:27:25 +0200

    upl

---
+++ src/middlewares/upload.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { TEMP_UPLOAD_DIR } from "../constants/constants.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, TEMP_UPLOAD_DIR);
+  },
+  filename: function (req, file, cb) {
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(null, `${uniquePrefix}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+export { upload };
