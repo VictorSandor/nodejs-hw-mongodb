341f579ef2e45f4561f662a0ccb227637988f462
Viktor S <sandorikviktor@gmail.com>
2025-01-04 18:02:28 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { initMongoConnection } from "./db/initMongoConnection.js";
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./constants/index.js";
-import { setupServer } from "./server.js";
+import { initMongoConection } from "./db/initMongoConnection.js";
+import setupServer from "./server.js";
 import { createDirIfNotExists } from "./utils/createDirIfNotExists.js";
 
-const boostrap = async () => {
-  await initMongoConnection();
+const bootstrap = async () => {
+  await initMongoConection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 
-void boostrap();
+void bootstrap();
